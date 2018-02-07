bc1324b91affacfb0bff05c7d3a4b649fa057cca
Cory Hogan <chogan314@users.noreply.github.com>
2018-02-06 23:55:32 -0600

    tightened up overlay for types with shorter names

--- website/scripts.js
+++ website/scripts.js
@@ -14,16 +14,39 @@ $(window).scroll(function() {
 
 var initialTriangleOverlayPoints = "100,0 100,16 83,0 83,0";
 var articleTriangleOverlayPoints = "100,0 100,9.5 76,9.5 76,0";
-var newsTriangleOverlayPoints = "100,0 100,9.5 76,9.5 76,0";
-var guideTriangleOverlayPoints = "100,0 100,9.5 76,9.5 76,0";
+var newsTriangleOverlayPoints = "100,0 100,9.5 78,9.5 78,0";
+var guideTriangleOverlayPoints = "100,0 100,9.5 77.5,9.5 77.5,0";
+
+function getArticleType(typeName) {
+    var type = "unknown";
+
+    if (typeName.parent().hasClass("entry-type-article")) {
+        type = "article";
+    } else if (typeName.parent().hasClass("entry-type-news")) {
+        type = "news";
+    } else if (typeName.parent().hasClass("entry-type-guide")) {
+        type = "guide";
+    }
+
+    return type;
+}
 
 $(".entry-image-container").mouseenter(function() {
     var imageOverlay =  $(this).find(".entry-image-overlay");
     var polygon = $(this).find(".entry-image-triangle-overlay").find("polygon");
     var typeName = $(this).find(".entry-type-name");
 
+    var articleType = getArticleType(typeName);
+    var overlayPoints = articleTriangleOverlayPoints;
+
+    if (articleType === "news") {
+        overlayPoints = newsTriangleOverlayPoints;
+    } else if (articleType === "guide") {
+        overlayPoints = guideTriangleOverlayPoints;
+    }
+
     imageOverlay.css("background-color", "rgba(211, 211, 211, 0.2)");
-    polygon.attr("points", articleTriangleOverlayPoints);
+    polygon.attr("points", overlayPoints);
     typeName.show();
 });
 
