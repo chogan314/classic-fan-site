583bda78d6cf10a441550a458e5f5f3c362b1897
Cory Hogan <chogan314@users.noreply.github.com>
2018-02-06 23:43:19 -0600

    overlay working correctly but no animation. javascript libraries can fuck themselves. not you jquery, you're cool

--- website/scripts.js
+++ website/scripts.js
@@ -12,16 +12,27 @@ $(window).scroll(function() {
     }
 });
 
+var initialTriangleOverlayPoints = "100,0 100,16 83,0 83,0";
+var articleTriangleOverlayPoints = "100,0 100,9.5 76,9.5 76,0";
+var newsTriangleOverlayPoints = "100,0 100,9.5 76,9.5 76,0";
+var guideTriangleOverlayPoints = "100,0 100,9.5 76,9.5 76,0";
+
 $(".entry-image-container").mouseenter(function() {
     var imageOverlay =  $(this).find(".entry-image-overlay");
-    var imageTriangleOverlay = $(this).find(".entry-image-triangle-overlay");
+    var polygon = $(this).find(".entry-image-triangle-overlay").find("polygon");
+    var typeName = $(this).find(".entry-type-name");
 
     imageOverlay.css("background-color", "rgba(211, 211, 211, 0.2)");
+    polygon.attr("points", articleTriangleOverlayPoints);
+    typeName.show();
 });
 
 $(".entry-image-container").mouseleave(function() {
     var imageOverlay =  $(this).find(".entry-image-overlay");
-    var imageTriangleOverlay = $(this).find(".entry-image-triangle-overlay");
+    var polygon = $(this).find(".entry-image-triangle-overlay").find("polygon");
+    var typeName = $(this).find(".entry-type-name");
 
     imageOverlay.css("background-color", "");
+    polygon.attr("points", initialTriangleOverlayPoints);
+    typeName.hide();
 });
