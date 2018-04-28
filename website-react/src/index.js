032b50d712df3491cba316a8a5aa62d49829ee72
Cory Hogan <chogan314@users.noreply.github.com>
2018-04-28 18:10:14 -0500

    fixed async rendering

--- website-react/src/index.js
+++ website-react/src/index.js
@@ -6,21 +6,55 @@ import ContentPreview from './components/ContentPreview';
 import './style/style.css';
 import previewData from './res/previewData.json';
 
-function init() {
-    return previewData;
+function init(index) {
+    var page = 0;
+    var pageSize = 12;
+
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = action;    
+    request.open('GET', 'php/get_content_previews.php');
+    request.send("pageSize=" + encodeURIComponent(page) + "&page=" + encodeURIComponent(pageSize));
+
+    index.state.previewData = previewData;
+    
+    function action() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                // index.state.previewData = JSON.parse(request.responseText);
+                index.setState({ previewData: JSON.parse(request.responseText) });
+            } else {
+                alert('There was a problem with the request.');
+            }
+        }
+    }
 }
 
 class Index extends Component {
     constructor(props) {
         super(props);
-        this.state = { previewData: init() };
+        this.state = {};
+    }
+
+    componentDidMount() {
+        init(this);
     }
 
     render() {
+        if (this.state.previewData) {
+            return(
+                <SiteContainer>
+                    <Grid>
+                        { Object.values(this.state.previewData).map(value => <ContentPreview key={value.id} data={value} />) }
+                    </Grid>
+                </SiteContainer>
+            );
+        }
+
         return(
             <SiteContainer>
                 <Grid>
-                    { Object.keys(this.state.previewData).map(key => <ContentPreview key={key} data={this.state.previewData[key]} />) }
+                    {/* { (this.state.previewData) ? Object.values(this.state.previewData).map(value => <ContentPreview key={value.id} data={value} />) : <div>???</div>} */}
+                    ??????
                 </Grid>
             </SiteContainer>
         );
