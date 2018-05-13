be7b69346fb3335852b6ec757f55417996e7cab6
Cory Hogan <chogan314@users.noreply.github.com>
2018-05-12 21:41:55 -0500

    fixed some mobile menu issues. probably a better way to implement this

--- website-react/src/components/site-container/Menu.jsx
+++ website-react/src/components/site-container/Menu.jsx
@@ -12,8 +12,7 @@ class Menu extends Component {
                         <Link to='/articles'><div className={"menu-item noselect" + (this.props.active === "articles" ? " active-menu-item" : "")}>Articles</div></Link>
                         <Link to='/guides'><div className={"menu-item noselect" + (this.props.active === "guides" ? " active-menu-item" : "")}>Guides</div></Link>
                         <Link to='/'><div className={"menu-item noselect" + (this.props.active === "tools" ? " active-menu-item" : "")}>Tools</div></Link>
-                        <div className="menu-hamburger-icon"></div>
-                        <div className="menu-close-icon"></div>
+                        <div className="mobile-menu-icon menu-open-icon"></div>
                     </div>
                 </div>
             </div>
--- website-react/src/components/site-container/MobileMenu.jsx
+++ website-react/src/components/site-container/MobileMenu.jsx
@@ -31,89 +31,62 @@ var menuOpen = false;
 var mainScroll = 0;
 var menuScroll = 0;
 
-document.addEventListener("DOMContentLoaded", function() {
-    var menu = document.getElementById("menu-section");
+function openMobileMenu() {
+    menuOpen = true;
+    mainScroll = window.scrollY;
 
-    Array.from(document.getElementsByClassName("menu-hamburger-icon")).forEach(function(element) {
-        element.onclick = function() {
-            menuOpen = true;
-            mainScroll = window.scrollY;
-            if (mainScroll < menu.offsetTop) {
-                if (menuScroll <= menu.offsetTop) {
-                    menuScroll = mainScroll;
-                }
-            } else {
-                if (menuScroll < menu.offsetTop) {
-                    menuScroll = menu.offsetTop;
-                }
-            }
-
-            document.getElementById("main-content").style.display = "none";
-            document.getElementById("mobile-menu-section").style.display = "block";
-            Array.from(document.getElementsByClassName("menu-hamburger-icon")).forEach(function(element) {
-                element.style.display = "none";
-            });
-            Array.from(document.getElementsByClassName("menu-close-icon")).forEach(function(element) {
-                element.style.display = "block";
-            });
-            window.scrollTo(0, menuScroll);
-
-            if (window.scrollY === 0) {
-                if (mainScroll >= menu.offsetTop) {
-                    document.getElementById("title-section").style.display = "none";
-                }
-            }
-        };
-    });
-
-    Array.from(document.getElementsByClassName("menu-close-icon")).forEach(function(element) {
-        element.onclick = function() {
-            menuOpen = false;
-            menuScroll = window.scrollY;
-            if (menuScroll < menu.offsetTop) {
-                if (mainScroll <= menu.offsetTop) {
-                    mainScroll = menuScroll;
-                }
-            } else {
-                if (mainScroll < menu.offsetTop) {
-                    mainScroll = menu.offsetTop;
-                }
-            }
-
-            document.getElementById("title-section").style.display = "block";
-            document.getElementById("main-content").style.display = "block";
-            document.getElementById("mobile-menu-section").style.display = "none";
-            Array.from(document.getElementsByClassName("menu-hamburger-icon")).forEach(function(element) {
-                element.style.display = "block";
-            });
-            Array.from(document.getElementsByClassName("menu-close-icon")).forEach(function(element) {
-                element.style.display = "none";
-            });
-            window.scrollTo(0, mainScroll);
-        };
-    });
-}, false);
-
-function onResize(event) {
-    if (window.innerWidth > 640 && window.innerWidth < 1440) {
-        menuOpen = false;
-        document.getElementById("title-section").style.display = "block";
-        document.getElementById("main-content").style.display = "block";
-        document.getElementById("mobile-menu-section").style.display = "none";
-        Array.from(document.getElementsByClassName("menu-hamburger-icon")).forEach(function (element) {
-            element.style.display = "none";
-        });
-        Array.from(document.getElementsByClassName("menu-close-icon")).forEach(function (element) {
-            element.style.display = "none";
-        });
-    } else if (window.innerWidth <= 640 && !menuOpen) {
-        Array.from(document.getElementsByClassName("menu-hamburger-icon")).forEach(function (element) {
-            element.style.display = "block";
-        });
-    } else if (window.innerWidth >= 1440) {
+    document.getElementById("main-content").style.display = "none";
+    document.getElementById("mobile-menu-section").style.display = "block";
+    if (document.getElementById("fixed-menu-section").style.display === "block") {
         document.getElementById("title-section").style.display = "none";
     }
+
+    Array.from(document.getElementsByClassName("mobile-menu-icon")).forEach(function(element) {
+        element.classList.add("menu-close-icon");
+        element.classList.remove("menu-open-icon");
+    });
+
+    window.scrollTo(0, menuScroll);
 }
+
+function closeMobileMenu() {
+    menuOpen = false;
+    menuScroll = window.scrollY;
+
+    document.getElementById("main-content").style.display = "flex";
+    document.getElementById("mobile-menu-section").style.display = "none";
+    document.getElementById("title-section").style.display = "block";
+
+    Array.from(document.getElementsByClassName("mobile-menu-icon")).forEach(function(element) {
+        element.classList.remove("menu-close-icon");
+        element.classList.add("menu-open-icon");
+    });
+
+    window.scrollTo(0, mainScroll);
+}
+
+function onResize(event) {
+    if (window.innerWidth > 640 && menuOpen) {
+        closeMobileMenu();
+    }
+    else if (window.innerWidth >= 1440) {
+        document.getElementById("title-section").style.display = "none";
+    } else if (!menuOpen) {
+        document.getElementById("title-section").style.display = "block";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    Array.from(document.getElementsByClassName("mobile-menu-icon")).forEach(function(element) {
+        element.onclick = function() {
+            if (!menuOpen) {
+                openMobileMenu();
+            } else {
+                closeMobileMenu();
+            }
+        }
+    });
+}, false);
 
 window.addEventListener("resize", onResize);
 
